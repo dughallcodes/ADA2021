f67e5d5dfc39ebfdfddd69062ccd7763a877cd2f
darius.milea <dariusmilea@yahoo.com>
2021-05-06 20:08:16 +0300

    Added user_id to jwt token

--- Backend/UserService/users/views.py
+++ Backend/UserService/users/views.py
@@ -5,6 +5,20 @@ from .serializers import UserSerializer, CourierSerializer
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.core.exceptions import ObjectDoesNotExist
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token["uuid"] = user.id
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 
 class CourierViewSet(viewsets.ModelViewSet):
